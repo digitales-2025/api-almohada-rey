642b6d5e9848d643148114f2635dcb807800d900
Diego Rondón <furrtr4532@gmail.com>
2025-03-07 15:11:54 -0500

    feat: agregar módulos de configuración, Prisma, administración, correo y eventos al módulo principal

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import { AdminModule } from './modules/admin/admin.module';
+import { EmailModule } from './email/email.module';
+import { TypedEventEmitterModule } from './event-emitter/typed-event-emitter.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    AdminModule,
+    EmailModule,
+    EventEmitterModule.forRoot(),
+    TypedEventEmitterModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
