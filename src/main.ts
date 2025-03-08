e23469355d609b02e7423642928aa9c60933e403
Diego Rondón <furrtr4532@gmail.com>
2025-03-08 08:39:44 -0500

    feat: agregar configuración de CORS, validación global y documentación Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,50 @@
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    origin: [process.env.WEB_URL, process.env.WEB_URL_SHOP],
+    credentials: true,
+  });
+  app.use(cookieParser());
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Almohada del Rey API')
+    .setDescription('API for Almohada del Rey')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  document.tags = [
+    {
+      name: 'Admin Auth',
+      description: 'Operaciones de autenticación y autorización',
+    },
+    {
+      name: 'Admin Account',
+      description: 'Gestión de cuentas de administrador y configuraciones',
+    },
+    { name: 'Admin Users', description: 'Gestión de usuarios administradores' },
+    { name: 'Admin Audit', description: 'Gestión de auditoría' },
+  ];
+
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(parseInt(process.env.PORT));
 }
 bootstrap();
