974375080261f0590fd16f7c3f92e2a79d870114
alxFv <worfysito15@gmail.com>
2025-03-20 15:36:37 -0500

    feat: cambios detectados nueva funcionalidad en update-up-history.dto.ts

--- src/modules/admin/rooms/dto/update-up-history.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateUpdateHistoryDto } from './create-up-history.dto';
-
-export class UpdateUpdateHistoryDto extends PartialType(
-  CreateUpdateHistoryDto,
-) {}
