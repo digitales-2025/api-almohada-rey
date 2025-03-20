56e3eeb5696945357ac3ef2c07f99eb4411668c7
alxFv <worfysito15@gmail.com>
2025-03-20 15:36:24 -0500

    feat: cambios detectados nueva funcionalidad en format-data.interceptor.ts

--- src/modules/admin/rooms/controllers/format-data.interceptor.ts
+++ src/modules/admin/rooms/controllers/format-data.interceptor.ts
@@ -12,33 +12,38 @@ export class FormatDataInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const request = context.switchToHttp().getRequest();
 
-    // Formatear los campos booleanos
-    if (request.body.prescription) {
-      request.body.prescription = request.body.prescription === 'true';
+    // Formatear campos numéricos
+    if (request.body.number) {
+      request.body.number = Number(request.body.number);
+      if (isNaN(request.body.number)) {
+        throw new BadRequestException('number must be a valid integer');
+      }
     }
-    if (request.body.medicalLeave) {
-      request.body.medicalLeave = request.body.medicalLeave === 'true';
+    
+    if (request.body.guests) {
+      request.body.guests = Number(request.body.guests);
+      if (isNaN(request.body.guests)) {
+        throw new BadRequestException('guests must be a valid integer');
+      }
     }
-
-    // Formatear el campo updateHistory
-    if (request.body.updateHistory) {
-      try {
-        request.body.updateHistory = JSON.parse(request.body.updateHistory);
-      } catch {
-        throw new BadRequestException(
-          'updateHistory must be a valid JSON object',
-        );
+    
+    if (request.body.price) {
+      request.body.price = Number(request.body.price);
+      if (isNaN(request.body.price)) {
+        throw new BadRequestException('price must be a valid number');
+      }
+    }
+    
+    if (request.body.area) {
+      request.body.area = Number(request.body.area);
+      if (isNaN(request.body.area)) {
+        throw new BadRequestException('area must be a valid number');
       }
     }
 
-    // Formatear el campo medicalLeaveDays
-    if (request.body.medicalLeaveDays) {
-      request.body.medicalLeaveDays = Number(request.body.medicalLeaveDays);
-      if (isNaN(request.body.medicalLeaveDays)) {
-        throw new BadRequestException(
-          'medicalLeaveDays must be a valid number',
-        );
-      }
+    // Formatear el campo isActive si existe
+    if (request.body.isActive !== undefined) {
+      request.body.isActive = request.body.isActive === 'true';
     }
 
     // Eliminar la propiedad imageUpdates si no se proporciona ningún dato
