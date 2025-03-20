7fe90da41782e607a35b94b1fc060e185d30f955
alxFv <worfysito15@gmail.com>
2025-03-20 15:36:32 -0500

    feat: cambios detectados nueva funcionalidad en delete-up-history.dto.ts

--- src/modules/admin/rooms/dto/delete-up-history.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsArray, IsString } from 'class-validator';
-
-export class DeleteUpdateHistoryDto {
-  @ApiProperty({ type: [String] })
-  @IsArray()
-  @IsString({ each: true })
-  ids: string[];
-}
