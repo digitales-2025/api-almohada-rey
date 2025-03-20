5251b35a929f6012403ffb2346c0f62ba8427a02
Gonzalo <gonzalogut.99@gmail.com>
2025-03-20 17:30:05 -0500

    feat: reservation entity succesfully configured

--- src/modules/reservation/entities/reservation.entity.ts
+++ src/modules/reservation/entities/reservation.entity.ts
@@ -1,11 +1,22 @@
 import { BaseEntity } from 'src/prisma/src/abstract/base.entity';
+import { ReservationStatus } from './reservation-status.enum';
+// import { Customer } from '../../customer/entities/customer.entity';
+// import { Room } from '../../room/entities/room.entity';
+// import { User } from '../../user/entities/user.entity';
 
 export class Reservation extends BaseEntity {
   id: string;
-  name: string;
-  hotelName: string;
-  arrivalDate: Date;
-  departureDate: Date;
+  customerId: string;
+
+  roomId: string;
+  userId: string;
+  reservationDate: Date;
+  checkInDate: Date;
+  checkOutDate: Date;
+  status: ReservationStatus;
+  guests?: any; // Json type for list of companions
+  observations?: string;
+  isActive: boolean;
   createdAt: Date;
   updatedAt: Date;
 
@@ -17,12 +28,38 @@ export class Reservation extends BaseEntity {
   toJSON(): Record<string, any> {
     return {
       id: this.id,
-      name: this.name,
-      hotelName: this.hotelName,
-      arrivalDate: this.arrivalDate,
-      departureDate: this.departureDate,
+      customerId: this.customerId,
+      roomId: this.roomId,
+      userId: this.userId,
+      reservationDate: this.reservationDate,
+      checkInDate: this.checkInDate,
+      checkOutDate: this.checkOutDate,
+      status: this.status,
+      guests: this.guests,
+      observations: this.observations,
+      isActive: this.isActive,
       createdAt: this.createdAt,
       updatedAt: this.updatedAt,
     };
   }
 }
+
+// export class DetailedReservation extends Reservation {
+//   customer: Customer;
+//   room: Room;
+//   user: User;
+
+//   constructor(partial: Partial<DetailedReservation>) {
+//     super(partial);
+//     Object.assign(this, partial);
+//   }
+
+//   toJSON(): Record<string, any> {
+//     return {
+//       ...super.toJSON(),
+//       customer: this.customer,
+//       room: this.room,
+//       user: this.user,
+//     };
+//   }
+// }
