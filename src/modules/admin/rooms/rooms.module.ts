cb8dd7e7fc53774c8ccf24b5943c8d9b90e5bb84
alxFv <worfysito15@gmail.com>
2025-03-20 15:37:12 -0500

    feat: cambios detectados nueva funcionalidad en rooms.module.ts

---
+++ src/modules/admin/rooms/rooms.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+
+
+
+import { CloudflareModule } from 'src/cloudflare/cloudflare.module';
+import { RoomsController } from './controllers/rooms.controller';
+import { RoomsService } from './services/rooms.service';
+import { AuditModule } from '../audit/audit.module';
+import { CreateRoomUseCase, DeleteRoomsUseCase, ReactivateRoomUseCase, UpdateRoomUseCase } from './use-cases';
+import { RoomsRepository } from './repositories/rooms.repository';
+
+@Module({
+  controllers: [
+    RoomsController,
+  ],
+  imports: [AuditModule, CloudflareModule],
+  providers: [
+
+    // use cases rooms
+    RoomsRepository,
+    RoomsService,
+    CreateRoomUseCase,
+    UpdateRoomUseCase,  
+    DeleteRoomsUseCase,
+    ReactivateRoomUseCase
+
+  
+  ],
+  exports: [RoomsService],
+})
+export class RoomsModule {}
