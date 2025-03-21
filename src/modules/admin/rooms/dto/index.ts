5b079e489814606d9ab9ababe4bec8bb55069965
alxFv <worfysito15@gmail.com>
2025-03-21 10:39:32 -0500

    feat: cambios detectados nueva funcionalidad en index.ts

--- src/modules/admin/rooms/dto/index.ts
+++ src/modules/admin/rooms/dto/index.ts
@@ -1,3 +1,5 @@
 export * from './create-rooms.dto';
 export * from './update-rooms.dto';
 export * from './delete-rooms.dto';
+export * from './create-room-with-images.dto';
+export * from './update-room-with-images.dto';
