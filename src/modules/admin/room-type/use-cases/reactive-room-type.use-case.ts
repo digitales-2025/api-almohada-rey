fddd2668dd6e5a1b51d182aedf4554bdf052bd48
alxFv <worfysito15@gmail.com>
2025-03-22 11:55:27 -0500

    feat: cambios detectados nueva funcionalidad en reactive-room-type.use-case.ts

--- src/modules/admin/room-type/use-cases/reactive-room-type.use-case.ts
+++ src/modules/admin/room-type/use-cases/reactive-room-type.use-case.ts
@@ -1,33 +1,33 @@
 import { Injectable } from '@nestjs/common';
-import { RoomsRepository } from '../repositories/room-type.repository';
-import { Room } from '../entities/room-type.entity';
+import { RoomTypeRepository } from '../repositories/room-type.repository';
+import { RoomType } from '../entities/room-type.entity';
 import { AuditActionType } from '@prisma/client';
 import { UserData } from 'src/interfaces';
 import { AuditService } from '../../audit/audit.service';
 import { BaseApiResponse } from 'src/utils/base-response/BaseApiResponse.dto';
 
 @Injectable()
-export class ReactivateRoomUseCase {
+export class ReactivateRoomTypeUseCase {
   constructor(
-    private readonly roomsRepository: RoomsRepository,
+    private readonly roomTypeRepository: RoomTypeRepository,
     private readonly auditService: AuditService,
   ) {}
 
   async execute(
     ids: string[],
     user: UserData,
-  ): Promise<BaseApiResponse<Room[]>> {
-    // Reactivar las habitaciones y registrar auditoría
-    const reactivatedRooms = await this.roomsRepository.transaction(
+  ): Promise<BaseApiResponse<RoomType[]>> {
+    // Reactivar los tipos de habitación y registrar auditoría
+    const reactivatedRoomTypes = await this.roomTypeRepository.transaction(
       async () => {
-        const rooms = await this.roomsRepository.reactivateMany(ids);
+        const roomTypes = await this.roomTypeRepository.reactivateMany(ids);
 
-        // Registrar auditoría para cada habitación reactivada
+        // Registrar auditoría para cada tipo de habitación reactivado
         await Promise.all(
-          rooms.map((room) =>
+          roomTypes.map((roomType) =>
             this.auditService.create({
-              entityId: room.id,
-              entityType: 'room',
+              entityId: roomType.id,
+              entityType: 'roomType',
               action: AuditActionType.UPDATE,
               performedById: user.id,
               createdAt: new Date(),
@@ -35,14 +35,14 @@ export class ReactivateRoomUseCase {
           ),
         );
 
-        return rooms;
+        return roomTypes;
       },
     );
 
     return {
       success: true,
-      message: 'Habitaciones reactivadas exitosamente',
-      data: reactivatedRooms,
+      message: 'Tipos de habitación reactivados exitosamente',
+      data: reactivatedRoomTypes,
     };
   }
 }
