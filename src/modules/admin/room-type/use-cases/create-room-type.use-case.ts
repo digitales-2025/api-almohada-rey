9a2b39e70aa807db464f7b8809c27387f64fce50
alxFv <worfysito15@gmail.com>
2025-03-22 11:55:21 -0500

    feat: cambios detectados nueva funcionalidad en create-room-type.use-case.ts

--- src/modules/admin/room-type/use-cases/create-room-type.use-case.ts
+++ src/modules/admin/room-type/use-cases/create-room-type.use-case.ts
@@ -1,54 +1,51 @@
 import { Injectable } from '@nestjs/common';
-import { CreateRoomDto } from '../dto/create-room-type.dto';
-import { Room } from '../entities/room-type.entity';
-import { RoomsRepository } from '../repositories/room-type.repository';
+import { CreateRoomTypeDto } from '../dto/create-room-type.dto';
+import { RoomType } from '../entities/room-type.entity';
+import { RoomTypeRepository } from '../repositories/room-type.repository';
 import { AuditActionType } from '@prisma/client';
 import { UserData } from 'src/interfaces';
 import { AuditService } from '../../audit/audit.service';
 import { BaseApiResponse } from 'src/utils/base-response/BaseApiResponse.dto';
 
 @Injectable()
-export class CreateRoomUseCase {
+export class CreateRoomTypeUseCase {
   constructor(
-    private readonly roomsRepository: RoomsRepository,
+    private readonly roomTypeRepository: RoomTypeRepository,
     private readonly auditService: AuditService,
   ) {}
 
   async execute(
-    createRoomDto: CreateRoomDto,
+    createRoomTypeDto: CreateRoomTypeDto,
     user: UserData,
-  ): Promise<BaseApiResponse<Room>> {
-    const newRoom = await this.roomsRepository.transaction(async () => {
-      // Crear habitación
-      const room = await this.roomsRepository.create({
-        number: createRoomDto.number,
-        guests: createRoomDto.guests,
-        type: createRoomDto.type,
-        price: createRoomDto.price,
-        status: createRoomDto.status,
-        tv: createRoomDto.tv,
-        floorType: createRoomDto.floorType,
-        description: createRoomDto.description,
-        area: createRoomDto.area,
-        bed: createRoomDto.bed,
+  ): Promise<BaseApiResponse<RoomType>> {
+    const newRoomType = await this.roomTypeRepository.transaction(async () => {
+      // Crear tipo de habitación
+      const roomType = await this.roomTypeRepository.create({
+        guests: createRoomTypeDto.guests,
+        price: createRoomTypeDto.price,
+        tv: createRoomTypeDto.tv,
+        floorType: createRoomTypeDto.floorType,
+        description: createRoomTypeDto.description,
+        area: createRoomTypeDto.area,
+        bed: createRoomTypeDto.bed,
       });
 
       // Registrar auditoría
       await this.auditService.create({
-        entityId: room.id,
-        entityType: 'room',
+        entityId: roomType.id,
+        entityType: 'roomType',
         action: AuditActionType.CREATE,
         performedById: user.id,
         createdAt: new Date(),
       });
 
-      return room;
+      return roomType;
     });
 
     return {
       success: true,
-      message: 'Habitación creada exitosamente',
-      data: newRoom,
+      message: 'Tipo de habitación creado exitosamente',
+      data: newRoomType,
     };
   }
 }
