4e702ea44211c3775b71fcab5b5e00e7a179dac2
alxFv <worfysito15@gmail.com>
2025-03-22 11:55:24 -0500

    feat: cambios detectados nueva funcionalidad en delete-room-type.use-case.ts

--- src/modules/admin/room-type/use-cases/delete-room-type.use-case.ts
+++ src/modules/admin/room-type/use-cases/delete-room-type.use-case.ts
@@ -1,51 +1,53 @@
 import { Injectable } from '@nestjs/common';
-import { RoomsRepository } from '../repositories/room-type.repository';
-import { Room } from '../entities/room-type.entity';
+import { RoomTypeRepository } from '../repositories/room-type.repository';
+import { RoomType } from '../entities/room-type.entity';
 
 import { AuditActionType } from '@prisma/client';
-import { DeleteRoomDto } from '../dto/delete-room-type.dto';
+import { DeleteRoomTypeDto } from '../dto/delete-room-type.dto';
 
 import { UserData } from 'src/interfaces';
 import { AuditService } from '../../audit/audit.service';
 import { BaseApiResponse } from 'src/utils/base-response/BaseApiResponse.dto';
 
 @Injectable()
-export class DeleteRoomsUseCase {
+export class DeleteRoomTypesUseCase {
   constructor(
-    private readonly roomsRepository: RoomsRepository,
+    private readonly roomTypeRepository: RoomTypeRepository,
     private readonly auditService: AuditService,
   ) {}
 
   async execute(
-    deleteRoomDto: DeleteRoomDto,
+    deleteRoomTypeDto: DeleteRoomTypeDto,
     user: UserData,
-  ): Promise<BaseApiResponse<Room[]>> {
-    const deletedRooms = await this.roomsRepository.transaction(async () => {
-      // Realiza el soft delete y obtiene las habitaciones desactivadas
-      const rooms = await this.roomsRepository.softDeleteMany(
-        deleteRoomDto.ids,
-      );
+  ): Promise<BaseApiResponse<RoomType[]>> {
+    const deletedRoomTypes = await this.roomTypeRepository.transaction(
+      async () => {
+        // Realiza el soft delete y obtiene los tipos de habitación desactivados
+        const roomTypes = await this.roomTypeRepository.softDeleteMany(
+          deleteRoomTypeDto.ids,
+        );
 
-      // Registra la auditoría para cada habitación eliminada
-      await Promise.all(
-        rooms.map((room) =>
-          this.auditService.create({
-            entityId: room.id,
-            entityType: 'room',
-            action: AuditActionType.DELETE,
-            performedById: user.id,
-            createdAt: new Date(),
-          }),
-        ),
-      );
+        // Registra la auditoría para cada tipo de habitación eliminado
+        await Promise.all(
+          roomTypes.map((roomType) =>
+            this.auditService.create({
+              entityId: roomType.id,
+              entityType: 'roomType',
+              action: AuditActionType.DELETE,
+              performedById: user.id,
+              createdAt: new Date(),
+            }),
+          ),
+        );
 
-      return rooms;
-    });
+        return roomTypes;
+      },
+    );
 
     return {
       success: true,
-      message: 'Habitaciones desactivadas exitosamente',
-      data: deletedRooms,
+      message: 'Tipos de habitación desactivados exitosamente',
+      data: deletedRoomTypes,
     };
   }
 }
