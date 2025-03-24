becdfa71082051e6fdeee5ed5566862eeaecfbb6
DiegoRondonR <furrtr4532@gmail.com>
2025-03-24 11:39:49 -0500

    feat: agregar interfaz ProductData para la gestión de productos

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,4 +2,6 @@ export { UserPayload, UserData, UserDataLogin } from './user.type';
 
 export { CustomerData } from './customer.interface';
 
+export { ProductData } from './product.interface';
+
 export { HttpResponse } from './https-response.interface';
