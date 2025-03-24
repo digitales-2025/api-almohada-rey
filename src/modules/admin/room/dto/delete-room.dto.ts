0631ed224559d79d958ede5fd416df4a71131631
alxFv <worfysito15@gmail.com>
2025-03-24 09:05:09 -0500

    feat: cambios detectados nueva funcionalidad en delete-room.dto.ts

--- src/modules/admin/room/dto/delete-room.dto.ts
+++ src/modules/admin/room/dto/delete-room.dto.ts
@@ -1,9 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsArray, IsString } from 'class-validator';
+import { IsArray, IsNotEmpty, IsString } from 'class-validator';
 
-export class DeletePrescriptionDto {
-  @ApiProperty({ type: [String] })
+export class DeleteRoomDto {
+  @ApiProperty({
+    description: 'IDs de las habitaciones a eliminar',
+    type: [String],
+    example: ['123e4567-e89b-12d3-a456-426614174000'],
+  })
   @IsArray()
   @IsString({ each: true })
+  @IsNotEmpty()
   ids: string[];
 }
