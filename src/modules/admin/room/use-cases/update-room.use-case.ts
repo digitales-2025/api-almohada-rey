5631eca903a521f8eaf852927ec1ed2d46a3c2f2
alxFv <worfysito15@gmail.com>
2025-03-24 09:05:38 -0500

    feat: cambios detectados nueva funcionalidad en update-room.use-case.ts

--- src/modules/admin/room/use-cases/update-room.use-case.ts
+++ src/modules/admin/room/use-cases/update-room.use-case.ts
@@ -1,58 +1,47 @@
 import { Injectable } from '@nestjs/common';
-import { UpdatePrescriptionDto } from '../dto/update-room.dto';
-import { Prescription } from '../entities/room.entity';
-import { PrescriptionRepository } from '../repositories/room.repository';
-import { UserData } from '@login/login/interfaces';
-import { AuditService } from '@login/login/admin/audit/audit.service';
+import { UpdateRoomDto } from '../dto/update-room.dto';
+import { Room } from '../entities/room.entity';
+import { RoomRepository } from '../repositories/room.repository';
 import { AuditActionType } from '@prisma/client';
-import { BaseApiResponse } from 'src/dto/BaseApiResponse.dto';
+import { UserData } from 'src/interfaces';
+import { AuditService } from '../../audit/audit.service';
+import { BaseApiResponse } from 'src/utils/base-response/BaseApiResponse.dto';
 
 @Injectable()
-export class UpdatePrescriptionUseCase {
+export class UpdateRoomUseCase {
   constructor(
-    private readonly prescriptionRepository: PrescriptionRepository,
+    private readonly roomRepository: RoomRepository,
     private readonly auditService: AuditService,
   ) {}
 
   async execute(
     id: string,
-    updatePrescriptionDto: UpdatePrescriptionDto,
+    updateRoomDto: UpdateRoomDto,
     user: UserData,
-  ): Promise<BaseApiResponse<Prescription>> {
-    const updatedPrescription = await this.prescriptionRepository.transaction(
-      async () => {
-        // Update prescription
-        const prescription = await this.prescriptionRepository.update(id, {
-          updateHistoryId: updatePrescriptionDto.updateHistoryId,
-          branchId: updatePrescriptionDto.branchId,
-          staffId: updatePrescriptionDto.staffId,
-          patientId: updatePrescriptionDto.patientId,
-          registrationDate: updatePrescriptionDto.registrationDate,
-          prescriptionMedicaments:
-            updatePrescriptionDto.prescriptionMedicaments,
-          prescriptionServices: updatePrescriptionDto.prescriptionServices,
-          description: updatePrescriptionDto.description,
-          purchaseOrderId: updatePrescriptionDto.purchaseOrderId,
-          isActive: true,
-        });
+  ): Promise<BaseApiResponse<Room>> {
+    const updatedRoom = await this.roomRepository.transaction(async () => {
+      // Actualizar habitación
+      const room = await this.roomRepository.update(id, {
+        type: updateRoomDto.type,
+        number: updateRoomDto.number,
+      });
 
-        // Register audit
-        await this.auditService.create({
-          entityId: prescription.id,
-          entityType: 'prescription',
-          action: AuditActionType.UPDATE,
-          performedById: user.id,
-          createdAt: new Date(),
-        });
+      // Registrar auditoría
+      await this.auditService.create({
+        entityId: room.id,
+        entityType: 'room',
+        action: AuditActionType.UPDATE,
+        performedById: user.id,
+        createdAt: new Date(),
+      });
 
-        return prescription;
-      },
-    );
+      return room;
+    });
 
     return {
       success: true,
-      message: 'Receta médica actualizada exitosamente',
-      data: updatedPrescription,
+      message: 'Habitación actualizada exitosamente',
+      data: updatedRoom,
     };
   }
 }
