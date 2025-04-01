f911a43ed57985eb5a5496e39985322691ebdcd0
alxFv <worfysito15@gmail.com>
2025-04-01 13:16:57 -0500

    feat: cambios detectados nueva funcionalidad en service-error.handler.ts

--- src/utils/error-handlers/service-error.handler.ts
+++ src/utils/error-handlers/service-error.handler.ts
@@ -2,6 +2,7 @@ import { BadRequestException, Logger, NotFoundException } from '@nestjs/common';
 import { roomCleanErrorMessages } from 'src/modules/admin/room-clean/errors/errors-room-clean';
 import { roomTypeErrorMessages } from 'src/modules/admin/room-type/errors/errors-room-type';
 import { roomErrorMessages } from 'src/modules/admin/room/errors/errors-room';
+import { serviceErrorMessages } from 'src/modules/admin/service/errors/errors-service';
 import { handleException } from 'src/utils';
 
 export type ServiceAction =
@@ -140,6 +141,7 @@ export const entityErrorMessages = {
   roomTypeErrorMessages,
   roomErrorMessages,
   roomCleanErrorMessages,
+  serviceErrorMessages,
   // Para agregar un nuevo módulo:
   // 1. Crear constante de mensajes de error siguiendo la interfaz ErrorMessages
   // 2. Agregar aquí con una clave apropiada
