5e459e763ea596ae9f739a23b0a836307fa0ceb0
DiegoRondonR <furrtr4532@gmail.com>
2025-05-03 12:16:33 -0500

    feat(users): agregar método para obtener usuarios paginados

--- src/modules/admin/users/users.module.ts
+++ src/modules/admin/users/users.module.ts
@@ -3,11 +3,12 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { AuditModule } from '../audit/audit.module';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { PaginationModule } from 'src/pagination/pagination.module';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
-  imports: [PrismaModule, AuditModule],
+  imports: [PrismaModule, AuditModule, PaginationModule],
   exports: [UsersService],
 })
 export class UsersModule {}
