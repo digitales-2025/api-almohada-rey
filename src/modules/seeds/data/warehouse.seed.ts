6f2568ffc0fe239afd4347c8d80ab0a07b93f87d
DiegoRondonR <furrtr4532@gmail.com>
2025-05-29 09:59:15 -0500

    feat(warehouse): cambiar tipo de columna 'type' a WarehouseType en el modelo de datos de almacenes

--- src/modules/seeds/data/warehouse.seed.ts
+++ src/modules/seeds/data/warehouse.seed.ts
@@ -1,17 +1,21 @@
-import { ProductType } from '@prisma/client';
+import { WarehouseType } from '@prisma/client';
 
 export type warehouseSeedData = {
-  type: ProductType;
+  type: WarehouseType;
   code: string;
 };
 
 export const warehousesSeed = [
   {
-    type: ProductType.COMMERCIAL,
+    type: WarehouseType.COMMERCIAL,
     code: 'ALM-COM-001',
   },
   {
-    type: ProductType.INTERNAL_USE,
+    type: WarehouseType.INTERNAL_USE,
     code: 'ALM-INT-001',
+  },
+  {
+    type: WarehouseType.DEPOSIT,
+    code: 'ALM-DEP-001',
   },
 ];
