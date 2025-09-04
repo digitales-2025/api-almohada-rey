96ffb0afddb7553e67fc7e64d6f4be94c288272d
DiegoRondonR <72468973@ucsm.edu.pe>
2025-09-04 16:08:51 -0500

    feat: update email templates and service with new branding and improved design

--- src/modules/seeds/data/superadmin.seed.ts
+++ src/modules/seeds/data/superadmin.seed.ts
@@ -3,8 +3,8 @@ import { UserRolType } from '@prisma/client';
 export const superAdminSeed = {
   name: 'Super Admin',
   email: 'admin@almohadarey.com',
-  password: 'admin123',
-  phone: '1234567890',
+  password: 'Almohada2025!1',
+  phone: '+51958959958',
   mustChangePassword: false,
   userRol: UserRolType.ADMIN,
 };
